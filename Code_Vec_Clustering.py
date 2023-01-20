e8c31efd08680ebc3da8587512e0afcc854fba65
Emanuele Fittipaldi <e.fittipaldi@studenti.unisa.it>
2023-01-20 13:14:02 +0100

    - Added the vector representation for every .java contained in every CWE folder in the JULIET Dataset. - All the vectors obtained from every .java were combined in a single vector as through the mean of the other vectors. - All the vectors are extracted with Code2Vec Code

--- Code_Vec_Clustering.py
+++ Code_Vec_Clustering.py
@@ -1,4 +1,5 @@
-import pandas as pd
+import os
+
 from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 import numpy as np
@@ -13,40 +14,29 @@ def cosineSimilarity(v1, v2):
     return cos_sim
 
 
-def strListTofloatList(v1):
-    v1 = v1.strip('[]')
-    v1 = v1.split(' ')
-    # deletes empty strings caused by the spaces between the values contained by the code vector
-    v1 = [string for string in v1 if string.strip() != '']
-    # delete the \n characters from the values containing them
-    v1 = [string.replace('\n', '') for string in v1]
-    # convert the string list into a float list
-    float_list = list(map(float, v1))
-    return float_list
+def applyPCA(n_components, vectors, n_clusters):
+    # Applying PCA in order to reduce the number of components from 384 to 2
+    pca = PCA(n_components=n_components)
+    code_vectors_2d = pca.fit_transform(vectors)
+
+    # Applying KMeans in order to identify the 3 clusters obtainable from the 2D vectors
+    kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(code_vectors_2d)
+
+    # Plotting the 2D vectors in order to get a glimpse on how these clusters looks like
+    fig = plt.gcf()
+    plt.figure(figsize=(14, 10), dpi=300)
+    plt.scatter(code_vectors_2d[:, 0], code_vectors_2d[:, 1], c=kmeans.labels_, s=0.7)
+    plt.show()
 
 
 vettori = []
-i = 0
-# This file is too big to commit, I'll upload a link where to download it
-path = "JULIET_Code_Vectors/Code_Vectors.csv"
-df = pd.read_csv(path)
+folder_path = 'JULIET_Code_Vectors'
 
-# Taking all the vectors contained in the csv
-for b in range(df.shape[0]):
-    vettori.append(strListTofloatList(df.iloc[i]['Code_Vectors']))
-    i += 1
+for filename in os.listdir(folder_path):
+    if filename.endswith('.npy'):
+        file_path = os.path.join(folder_path, filename)
+        data = np.load(file_path)
+        vettori.extend(data)
 
-# Applying PCA in order to reduce the number of components from 384 to 2
-pca = PCA(n_components=2)
-code_vectors_2d = pca.fit_transform(vettori)
-
-# Applying KMeans in order to identify the 3 clusters obtainable from the 2D vectors
-kmeans = KMeans(n_clusters=3, random_state=0).fit(code_vectors_2d)
-
-# Plotting the 2D vectors in order to get a glimpse on how these clusters looks like
-fig = plt.gcf()
-plt.figure(figsize=(14,10), dpi=300)
-plt.scatter(code_vectors_2d[:, 0], code_vectors_2d[:, 1], c=kmeans.labels_, s=0.01)
-plt.show()
-
-
+print(len(vettori))
+applyPCA(n_components=2, n_clusters=3, vectors=vettori)
